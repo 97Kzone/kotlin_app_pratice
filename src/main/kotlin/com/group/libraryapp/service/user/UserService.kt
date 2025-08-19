744f59c5be21659c20dedd96bd5b07477a762667
97Kzone <76652908+97Kzone@users.noreply.github.com>
2025-08-20 00:33:44 +0900

    25.08.20 DTO에 Builder 위임, 코드 정리

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -45,16 +45,7 @@ class UserService (
 
     @Transactional(readOnly = true)
     fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
-        return userRepository.findAllWitHistories().map { user ->
-            UserLoanHistoryResponse(
-                name = user.name,
-                books = user.userLoanHistories.map { history ->
-                    BookHistoryResponse(
-                        name = history.bookName,
-                        isReturn = history.status == UserLoanStatus.RETURNED
-                    )
-                }
-            )
-        }
+        return userRepository.findAllWitHistories()
+            .map(UserLoanHistoryResponse::of)
     }
 }
